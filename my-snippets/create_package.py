1b638aa67e5865fa813fc35c07cc3b0f71781b41
Ansh Ujlayan <anshujlayan3@gmail.com>
2024-06-06 19:06:32 +0530

    snippets added for C++

---
+++ my-snippets/create_package.py
@@ -0,0 +1,27 @@
+import os
+import json
+
+snippet_home = os.path.expanduser("~/.config/nvim/my-snippets")
+snippet_lang = ["cpp", "python"]
+snippets = list()
+
+for lang in snippet_lang:
+    snippet_dirs = os.path.join(snippet_home, lang)
+    for snippet_file_path in os.listdir(snippet_dirs):
+        if snippet_file_path.endswith(".json"):
+            path = os.path.join(snippet_dirs, snippet_file_path)
+            snippets.append({"language": lang, "path": os.path.relpath(path)})
+
+
+package_json = {
+    "name": "my-snippets",
+    "version": "1.0.0",
+    "description": "My custom snippets",
+    "main": "index.js",
+    "author": "Ansh Ujlayan",
+    "license": "ISC",
+    "contributes": {"snippets": snippets},
+}
+
+with open("package.json", "w") as f:
+    json.dump(package_json, f, indent=2)
