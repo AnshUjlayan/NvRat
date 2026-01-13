89d7c02b1cbf0766a8087a3e7277abfcef7e6578
AnshUjlayan <anshujlayan3@gmail.com>
2026-01-13 22:11:13 +0530

    fix: lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,19 +1,12 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lsp = vim.lsp.config
-local nvlsp = require "nvchad.configs.lspconfig"
-
--- common opts
-local opts = {
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
-}
-
--- servers with default config
+-- Language Servers
 local servers = {
+  "lua_ls",
+  "rust_analyzer",
   "ts_ls",
+  "clangd",
   "pyright",
   "html",
   "cssls",
@@ -23,39 +16,4 @@ local servers = {
   "docker_compose_language_service",
 }
 
-for _, server in ipairs(servers) do
-  lsp(server, opts)
-end
-
--- lua_ls
-lsp(
-  "lua_ls",
-  vim.tbl_deep_extend("force", opts, {
-    settings = {
-      Lua = {
-        diagnostics = { globals = { "vim" } },
-      },
-    },
-  })
-)
-
--- clangd
-lsp(
-  "clangd",
-  vim.tbl_deep_extend("force", opts, {
-    cmd = { "/usr/bin/clangd" },
-  })
-)
-
--- rust_analyzer
-lsp(
-  "rust_analyzer",
-  vim.tbl_deep_extend("force", opts, {
-    cmd = { "rustup", "run", "stable", "rust-analyzer" },
-    settings = {
-      ["rust-analyzer"] = {
-        cargo = { allFeatures = true },
-      },
-    },
-  })
-)
+vim.lsp.enable(servers)
